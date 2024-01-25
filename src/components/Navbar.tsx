82c1b523110bbf1d195a0a7cbcbc425c7510a842
Daniel He z5421214 <kyubyte@DANIEL-PC.localdomain>
2024-01-25 23:42:15 +1100

    Closer gap in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ export default function Navbar() {
   return (
     <div style={{
       display: 'flex',
-      padding: '10px 35px',
+      padding: '10px 10px',
       borderRadius: '15px',
       width: '1150px',
       maxWidth: '1150px',
-      justifyContent: 'space-between',
+      justifyContent: 'space-around',
       border: '1px solid #e5e7eb',
       backgroundColor: 'rgba(255,255,255,0.6)',
       backdropFilter: 'blur(8px)',
