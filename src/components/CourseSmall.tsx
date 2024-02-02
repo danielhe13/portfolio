d35fe7e756f68f7f24823776f29ea663b73bef8c
Daniel He z5421214 <kyubyte@DANIEL-PC.localdomain>
2024-02-02 23:00:19 +1100

    Change Course Panel to enlarge on Hover

--- src/components/CourseSmall.tsx
+++ src/components/CourseSmall.tsx
@@ -18,12 +18,13 @@ export default function CourseSmall({ course }: Props) {
         border: '1px solid #e5e7eb',
         borderRadius: 4,
         padding: open ? '25px 40px' : '20px 35px',
-        width: open ? '650px' : '450px',
+        width: open ? '550px' : '450px',
         boxShadow: '-3px 4px 4px 0px rgba(0, 0, 0, 0.10)',
         cursor: 'pointer',
-        transition: 'all 3s ease-in-out',
+        transition: 'all 0.5s ease-in-out',
       }}
-        onClick={() => setOpen(!open)}
+        onMouseOver={() => setOpen(true)}
+        onMouseOut={() => setOpen(false)}
       >
         <Typography variant={open ? 'h4' : 'h5'}>
           {course.name}
